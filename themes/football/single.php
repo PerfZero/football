0958a84f6e2ecc65c48995a2ecaea4d19e59f122
PerfZero <gundenis1@gmail.com>
2026-05-19 18:29:52 +0300

    Improve news single layout

--- themes/football/single.php
+++ themes/football/single.php
@@ -6,14 +6,24 @@ if (!defined('ABSPATH')) {
 get_header();
 ?>
 
-<main id="main" class="site-main">
-    <section class="container single-post">
-        <?php while (have_posts()) : ?>
-            <?php the_post(); ?>
-            <article <?php post_class(); ?>>
+<main id="main" class="site-main news-single">
+    <?php while (have_posts()) : ?>
+        <?php the_post(); ?>
+
+        <section class="single-post-hero">
+            <div class="container single-post-hero__inner">
+                <p class="breadcrumbs">
+                    <a href="<?php echo esc_url(home_url('/')); ?>"><?php football_esc_html_t('home'); ?></a>
+                    <span>/</span>
+                    <span><?php football_esc_html_t('section.news'); ?></span>
+                </p>
                 <p class="post-card__meta"><?php echo esc_html(get_the_date()); ?></p>
                 <h1><?php the_title(); ?></h1>
+            </div>
+        </section>
 
+        <section class="container single-post-layout">
+            <article <?php post_class('single-post-panel'); ?>>
                 <?php if (has_post_thumbnail()) : ?>
                     <div class="single-post__image">
                         <?php the_post_thumbnail('large'); ?>
@@ -24,8 +34,8 @@ get_header();
                     <?php the_content(); ?>
                 </div>
             </article>
-        <?php endwhile; ?>
-    </section>
+        </section>
+    <?php endwhile; ?>
 </main>
 
 <?php
